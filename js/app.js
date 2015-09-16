4ecd1674588181b15581b9def4f1b5f1960a9b5d
Anthony Quinn <quinn.ant@gmail.com>
2015-09-16 08:37:13 -0400

    Add a super GameSprite object. Refactor Enemy. Add Player. Add Reset methods. Got enemy to render and move across board with varying speeds. Add Player and capture movements.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,134 @@
+
+/* A GameSprite Object
+* A common parent object for game entities that need
+* position x, y
+* update function
+* render function
+*/
+var GameSprite = function(sprite){
+  this.x = 0;
+  this.y = 0;
+  this.moveX = 0;
+  this.moveY = 0;
+  this.sprite = sprite;
+};
+
+GameSprite.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+    GameSprite.call(this, "images/enemy-bug.png" );
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    //assign a random multiplier for the speed of the enemy.
+    this.velocity = getRandomIntInclusive(85, 202);
+
+    //Reset will place an enemy back at x=0 in a random row.
+    this.reset();
+
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+Enemy.prototype = Object.create(GameSprite.prototype);
+Enemy.prototype.constructor = GameSprite;
+Enemy.prototype.reset = function(loc){
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  //Set the three y cordinates where an enemy can begin
+  var startOptions = [60,145, 225];
+  //Get a random start location between 0 and 2
+  var rand = getRandomIntInclusive(0, 2);
+  //Set X cordinate to negative the velocity. This will keep it from looking like it jumps the first time
+  this.x =  this.velocity*-1;
+  //Get the random start row
+  this.y = startOptions[rand];
+};
+Enemy.prototype.update = function(dt){
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + this.velocity * dt;
+  if(this.x >= 601){
+    this.reset();
+  };
+  this.render();
+
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+  //Load player sprite image
+  GameSprite.call(this, "images/char-boy.png");
+  this.reset();
 
+};
+Player.prototype = Object.create(GameSprite.prototype);
+Player.prototype.constructor = GameSprite;
+// Read input from the keyboard and move player.
+// Check bountries
+Player.prototype.handleInput = function(input){
+  //dependingon movement direction set a move cordinate.
+  switch (input){
+    case "up":
+      this.moveY = -85;
+      break;
+    case "down":
+      this.moveY = 85;
+      break;
+    case "left":
+      this.moveX = -101;
+      break;
+    case "right":
+      this.moveX = 101;
+      break;
+  };
+  this.update();
+};
+// Move player back to initial starting point
+Player.prototype.reset = function(){
+  this.x = 200;
+  this.y = 390;
+
+};
+Player.prototype.update = function(){
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  if(this.moveX != 0){
+    //Update X movement
+    this.x = (this.x + this.moveX);
+    this.moveX = 0;
+  };
+  if(this.moveY != 0){
+    //Update Y movement
+    this.y = (this.y + this.moveY);
+    this.moveY = 0;
+  };
+
+  //Check for collisions
+  console.log("Check Collisions");
+  allEnemies.forEach(function(enemy) {
+    console.log("ex: "+ enemy.x+ " ey: "+ enemy.y);
+    console.log("px: " + player.x+ " py: "+ player.y);
+      if(enemy.x == player.x && enemy.y == player.y){
+
+        this.reset();
+      };
+  });
+
+  this.render();
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+
 // Place the player object in a variable called player
+var player = new Player();
 
 
 
@@ -44,3 +144,10 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+// Using from MDN to generate random starting row for Enemies.
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}

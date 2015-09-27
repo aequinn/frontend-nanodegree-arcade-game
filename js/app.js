949045f6257e0cd89a27e4e7c869e7a93ffbaedf
Anthony Quinn <quinn.ant@gmail.com>
2015-09-27 17:08:16 -0400

    Add collision code. Add checking boundary on player movement.

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ var GameSprite = function(sprite){
   this.moveX = 0;
   this.moveY = 0;
   this.sprite = sprite;
+  this.width;
+  this.height;
 };
 
 GameSprite.prototype.render = function(){
@@ -24,7 +26,7 @@ var Enemy = function() {
     GameSprite.call(this, "images/enemy-bug.png" );
 
     //assign a random multiplier for the speed of the enemy.
-    this.velocity = getRandomIntInclusive(85, 202);
+    this.velocity = getRandomIntInclusive(100, 402);
 
     //Reset will place an enemy back at x=0 in a random row.
     this.reset();
@@ -36,47 +38,54 @@ Enemy.prototype.constructor = GameSprite;
 Enemy.prototype.reset = function(loc){
 
   //Set the three y cordinates where an enemy can begin
-  var startOptions = [60,145, 225];
+  // about 83 spaces apart
+  var startOptions = [60,143, 226];
   //Get a random start location between 0 and 2
   var rand = getRandomIntInclusive(0, 2);
   //Set X cordinate to negative the velocity. This will keep it from looking like it jumps the first time
   this.x =  this.velocity*-1;
   //Get the random start row
   this.y = startOptions[rand];
+
 };
 Enemy.prototype.update = function(dt){
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
+
   this.x = this.x + this.velocity * dt;
   if(this.x >= 601){
     this.reset();
   };
+
   this.render();
 
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player Class and functions
+// Creates players and functions to handle the aspects of player
 var Player = function(){
   //Load player sprite image
-  GameSprite.call(this, "images/char-boy.png");
+  GameSprite.call(this, "images/char-princess-girl.png");
+  var img = Resources.get(this.sprite);
+  console.log(img);
+
   this.reset();
 
 };
 Player.prototype = Object.create(GameSprite.prototype);
 Player.prototype.constructor = GameSprite;
+
 // Read input from the keyboard and move player.
 // Check bountries
 Player.prototype.handleInput = function(input){
   //dependingon movement direction set a move cordinate.
   switch (input){
     case "up":
-      this.moveY = -85;
+      this.moveY = -83;
       break;
     case "down":
-      this.moveY = 85;
+      this.moveY = 83;
       break;
     case "left":
       this.moveX = -101;
@@ -89,40 +98,49 @@ Player.prototype.handleInput = function(input){
 };
 // Move player back to initial starting point
 Player.prototype.reset = function(){
-  this.x = 200;
-  this.y = 390;
+
+  this.x = 202;
+  this.y = 400;
 
 };
+
 Player.prototype.update = function(){
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  if(this.moveX != 0){
-    //Update X movement
-    this.x = (this.x + this.moveX);
-    this.moveX = 0;
+
+  //Check direction of movement in the x direction
+  if(this.moveX != 0 ){
+    //Check if moving off vertical boundary of the game board
+    if(this.nextMove(this.moveX, this.x)>500 || this.nextMove(this.moveX, this.x)<-5 ){
+      this.moveX = 0;
+    }else{
+      //Update X movement
+      this.x = this.nextMove(this.x, this.moveX);
+      this.moveX = 0;
+    };
   };
+
+  //Check direction of movement in the y direction
   if(this.moveY != 0){
-    //Update Y movement
-    this.y = (this.y + this.moveY);
-    this.moveY = 0;
+    //check if we will move past the bottom border, don't move if we do.
+    if(this.nextMove(this.y, this.moveY) >400){
+      this.moveY = 0;
+    }else if( this.nextMove(this.y, this.moveY) < 0){
+      //check to see if we move past top boundary, reset position if we do.
+      this.moveY = 0;
+      this.reset();
+    }else{
+      //If we are in bounds, Update Y movement
+      this.y = this.nextMove(this.y, this.moveY);
+      this.moveY = 0;
+    };
   };
-
-  //Check for collisions
-  console.log("Check Collisions");
-  allEnemies.forEach(function(enemy) {
-    console.log("ex: "+ enemy.x+ " ey: "+ enemy.y);
-    console.log("px: " + player.x+ " py: "+ player.y);
-      if(enemy.x == player.x && enemy.y == player.y){
-
-        this.reset();
-      };
-  });
 
   this.render();
-
 };
 
+//Function that adds current position with movement
+Player.prototype.nextMove = function(current, diff){
+  return current + diff;
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
@@ -130,7 +148,28 @@ var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 // Place the player object in a variable called player
 var player = new Player();
 
+//Checks for Collisions and updates entities accordingly
+//This includes the edges.
+var checkCollisions = function(){
 
+  console.log("Check Collisions");
+  //check the position of each enemy compared to the player to see if they occupy the same space.
+  allEnemies.forEach(function(enemy) {
+    /*To Do
+    * Check to see if ranges intersect at all with other objects to detect collisions
+    */
+
+    //Check collision with enemies
+    //Check if the right most corner of an enemy is over the left most corner of the player
+      if((enemy.x+101 > player.x+50 && enemy.x < player.x+50) ){
+        //Check if y cordinates overlap - Player between y+70 y+140 ; enemy y+80 y+145
+        if((enemy.y+80 >= player.y+70) && (enemy.y+145 <= player.y+140)){
+            player.reset();
+        }
+
+      };
+  });
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
